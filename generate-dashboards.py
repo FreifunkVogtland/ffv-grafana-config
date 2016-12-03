5496a6b9b89ea6ce74156a4444d3beac9a962461
Sven Eckelmann <sven@narfation.org>
2016-12-03 21:07:27 +0100

    Generate node dashboard with human readable names

--- generate-dashboards.py
+++ generate-dashboards.py
@@ -157,6 +157,26 @@ def generate_nodegroup(id_name_pairs, templatepath, outpath):
 	dump_json(dashboard, outfiletmp)
 	os.rename(outfiletmp, outfile)
 
+def generate_node(id_name_pairs, templatepath, outpath):
+	dashboard = json.load(open(os.path.join(templatepath, "node.json")))
+	filename = 'node.json'
+
+	dashboard['templating']['list'][0]['options'] = []
+	options = []
+	for p in id_name_pairs:
+		option = {
+			"value": p['id'],
+			"text": p['name']
+		}
+		options.append(option)
+
+	dashboard['templating']['list'][0]['options'] = sorted(options, key=lambda k: k['text'])
+	outfile = os.path.join(outpath, filename)
+	outfiletmp = os.path.join(outpath, '%s.tmp' % (filename))
+
+	dump_json(dashboard, outfiletmp)
+	os.rename(outfiletmp, outfile)
+
 def main():
 	if len(sys.argv) != 4:
 		print("./generate-dashboards.py NODELIST TEMPLATEPATH OUTPATH")
@@ -173,6 +193,7 @@ def main():
 	# store
 	generate_places(id_name_pairs, templatepath, outpath)
 	generate_nodegroup(id_name_pairs, templatepath, outpath)
+	generate_node(id_name_pairs, templatepath, outpath)
 
 if __name__ == "__main__":
 	main()
