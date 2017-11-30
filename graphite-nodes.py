ad5c8a59c783df1f75a5997b1e941bcfd416168c
Sven Eckelmann <sven@narfation.org>
2017-11-30 17:26:53 +0100

    Add global offline nodes and available nodes information

--- graphite-nodes.py
+++ graphite-nodes.py
@@ -5,8 +5,9 @@ import sys
 import json
 import time
 
-def get_onlinenodes_count(nodes):
+def get_nodes_count(nodes):
 	onlinenodes_count = 0
+	offlinenodes_count = 0
 
 	for n in nodes['nodes']:
 		if not 'statistics' in n:
@@ -19,11 +20,11 @@ def get_onlinenodes_count(nodes):
 			continue
 
 		if not n['flags']['online']:
-			continue
+			offlinenodes_count += 1
+		else:
+			onlinenodes_count += 1
 
-		onlinenodes_count += 1
-
-	return onlinenodes_count
+	return (onlinenodes_count, offlinenodes_count)
 
 def main():
 	if len(sys.argv) != 2:
@@ -34,10 +35,12 @@ def main():
 
 	# load
 	nodes = json.load(open(nodesjson))
-	onlinenodes_count = get_onlinenodes_count(nodes)
+	(onlinenodes_count, offlinenodes_count) = get_nodes_count(nodes)
 
 	timestamp = int(time.time())
 	print("freifunk.global.onlinenodes %u %u" % (onlinenodes_count, timestamp))
+	print("freifunk.global.offlinenodes %u %u" % (offlinenodes_count, timestamp))
+	print("freifunk.global.nodes %u %u" % (onlinenodes_count + offlinenodes_count, timestamp))
 
 if __name__ == "__main__":
 	main()
