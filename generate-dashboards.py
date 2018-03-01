9d5fbc76b923eca6a4a31a9306b45e674523ddf3
Sven Eckelmann <sven@narfation.org>
2018-03-01 22:09:18 +0100

    Fix PEP8 coding style problems

--- generate-dashboards.py
+++ generate-dashboards.py
@@ -2,161 +2,174 @@
 # -*- coding: utf-8; -*-
 
 import copy
-import sys
-import os, os.path
 import json
+import os
+import os.path
 import re
+import sys
+
 
 def dump_json(data, filename):
-	with open(filename, 'w') as f:
-		json.dump(data, f)
-		f.flush()
-		os.fsync(f.fileno())
+    with open(filename, 'w') as f:
+        json.dump(data, f)
+        f.flush()
+        os.fsync(f.fileno())
+
 
 def asciify(s):
-	return s.encode('ascii', 'replace').decode('ascii')
+    return s.encode('ascii', 'replace').decode('ascii')
+
 
 def get_id_name_pairs(nodelistjson):
-	id_name_pairs = []
+    id_name_pairs = []
 
-	for node in nodelistjson['nodes']:
-		if not 'id' in node:
-			continue
+    for node in nodelistjson['nodes']:
+        if 'id' not in node:
+            continue
 
-		if not 'name' in node:
-			continue
+        if 'name' not in node:
+            continue
 
-		id_pair = {
-			'id': node['id'],
-			'name': asciify(node['name']),
-		}
-		id_name_pairs.append(id_pair)
+        id_pair = {
+            'id': node['id'],
+            'name': asciify(node['name']),
+        }
+        id_name_pairs.append(id_pair)
 
-	return id_name_pairs
+    return id_name_pairs
+
 
 def filter_id_name_pairs(id_name_pairs, regex):
-	r = re.compile(regex)
-	return list(filter(lambda n: r.match(n['name']), id_name_pairs))
+    r = re.compile(regex)
+    return list(filter(lambda n: r.match(n['name']), id_name_pairs))
+
 
 def generate_place_dashboard(id_name_pairs, place_template, title, tags):
-	place = copy.deepcopy(place_template)
-	id_array = [p['id'] for p in id_name_pairs]
-	id_str = ','.join(id_array)
+    place = copy.deepcopy(place_template)
+    id_array = [p['id'] for p in id_name_pairs]
+    id_str = ','.join(id_array)
 
-	place['title'] = title
-	place['tags'] = tags
-	place['rows'][0]['panels'][0]['targets'][0]['target'] = "alias(sumSeries(freifunk.node.{%s}.*.clients.total), 'Nutzer')" % (id_str)
+    place['title'] = title
+    place['tags'] = tags
+    place['rows'][0]['panels'][0]['targets'][0]['target'] = "alias(sumSeries(freifunk.node.{%s}.*.clients.total), 'Nutzer')" % (id_str)
 
-	place['rows'][0]['panels'][1]['targets'] = []
-	for p in id_name_pairs:
-		target = {
-			"refId": p['id'],
-			"target": "alias(sumSeries(freifunk.node.%s.*.clients.total), '%s')" % (p['id'], p['name'])
-		}
-		place['rows'][0]['panels'][1]['targets'].append(target)
+    place['rows'][0]['panels'][1]['targets'] = []
+    for p in id_name_pairs:
+        target = {
+            "refId": p['id'],
+            "target": "alias(sumSeries(freifunk.node.%s.*.clients.total), '%s')" % (p['id'], p['name'])
+        }
+        place['rows'][0]['panels'][1]['targets'].append(target)
 
-	place['rows'][0]['panels'][2]['targets'][0]['target'] = "alias(sumSeries(nonNegativeDerivative(freifunk.node.{%s}.*.traffic.rx.bytes)), 'Download')" % (id_str)
-	place['rows'][0]['panels'][2]['targets'][1]['target'] = "alias(sumSeries(nonNegativeDerivative(freifunk.node.{%s}.*.traffic.tx.bytes)), 'Upload')" % (id_str)
-	place['rows'][0]['panels'][2]['targets'][2]['target'] = "alias(sumSeries(nonNegativeDerivative(freifunk.node.{%s}.*.traffic.forward.bytes)), 'Mesh')" % (id_str)
+    place['rows'][0]['panels'][2]['targets'][0]['target'] = "alias(sumSeries(nonNegativeDerivative(freifunk.node.{%s}.*.traffic.rx.bytes)), 'Download')" % (id_str)
+    place['rows'][0]['panels'][2]['targets'][1]['target'] = "alias(sumSeries(nonNegativeDerivative(freifunk.node.{%s}.*.traffic.tx.bytes)), 'Upload')" % (id_str)
+    place['rows'][0]['panels'][2]['targets'][2]['target'] = "alias(sumSeries(nonNegativeDerivative(freifunk.node.{%s}.*.traffic.forward.bytes)), 'Mesh')" % (id_str)
 
-	return place
+    return place
+
 
 def generate_places(place_filter, id_name_pairs, templatepath, outpath):
-	place_template = json.load(open(os.path.join(templatepath, "place.json")))
+    place_template = json.load(open(os.path.join(templatepath, "place.json")))
 
-	for title in place_filter:
-		regex = place_filter[title]['regex']
-		filename = place_filter[title]['filename']
-		tags = place_filter[title]['tags']
+    for title in place_filter:
+        regex = place_filter[title]['regex']
+        filename = place_filter[title]['filename']
+        tags = place_filter[title]['tags']
 
-		place_pairs = filter_id_name_pairs(id_name_pairs, regex)
+        place_pairs = filter_id_name_pairs(id_name_pairs, regex)
 
-		dashboard = generate_place_dashboard(place_pairs, place_template, title, tags)
+        dashboard = generate_place_dashboard(place_pairs, place_template,
+                                             title, tags)
 
-		outfile = os.path.join(outpath, filename)
-		outfiletmp = os.path.join(outpath, '%s.tmp' % (filename))
+        outfile = os.path.join(outpath, filename)
+        outfiletmp = os.path.join(outpath, '%s.tmp' % (filename))
 
-		dump_json(dashboard, outfiletmp)
-		os.rename(outfiletmp, outfile)
+        dump_json(dashboard, outfiletmp)
+        os.rename(outfiletmp, outfile)
+
 
 def generate_nodegroup(id_name_pairs, templatepath, outpath):
-	dashboard = json.load(open(os.path.join(templatepath, "nodegroup.json")))
-	filename = 'nodegroup.json'
+    dashboard = json.load(open(os.path.join(templatepath, "nodegroup.json")))
+    filename = 'nodegroup.json'
 
-	dashboard['templating']['list'][0]['options'] = []
-	options = []
-	for p in id_name_pairs:
-		option = {
-			"value": p['id'],
-			"text": "%s (%s)" % (p['name'], p['id']),
-			"selected": False
-		}
-		options.append(option)
+    dashboard['templating']['list'][0]['options'] = []
+    options = []
+    for p in id_name_pairs:
+        option = {
+            "value": p['id'],
+            "text": "%s (%s)" % (p['name'], p['id']),
+            "selected": False
+        }
+        options.append(option)
 
-	options = sorted(options, key=lambda k: k['text'])
-	if options:
-		options[0]['selected'] = True
-		dashboard['templating']['list'][0]['current'] = {
-			"tags": [],
-			"text": options[0]["text"],
-			"value": options[0]["value"]
-		}
+    options = sorted(options, key=lambda k: k['text'])
+    if options:
+        options[0]['selected'] = True
+        dashboard['templating']['list'][0]['current'] = {
+            "tags": [],
+            "text": options[0]["text"],
+            "value": options[0]["value"]
+        }
 
-	dashboard['templating']['list'][0]['options'] = options
-	outfile = os.path.join(outpath, filename)
-	outfiletmp = os.path.join(outpath, '%s.tmp' % (filename))
+    dashboard['templating']['list'][0]['options'] = options
+    outfile = os.path.join(outpath, filename)
+    outfiletmp = os.path.join(outpath, '%s.tmp' % (filename))
 
-	dump_json(dashboard, outfiletmp)
-	os.rename(outfiletmp, outfile)
+    dump_json(dashboard, outfiletmp)
+    os.rename(outfiletmp, outfile)
+
 
 def generate_node(id_name_pairs, templatepath, outpath):
-	dashboard = json.load(open(os.path.join(templatepath, "node.json")))
-	filename = 'node.json'
+    dashboard = json.load(open(os.path.join(templatepath, "node.json")))
+    filename = 'node.json'
 
-	dashboard['templating']['list'][0]['options'] = []
-	options = []
-	for p in id_name_pairs:
-		option = {
-			"value": p['id'],
-			"text": p['name'],
-			"selected": False
-		}
-		options.append(option)
+    dashboard['templating']['list'][0]['options'] = []
+    options = []
+    for p in id_name_pairs:
+        option = {
+            "value": p['id'],
+            "text": p['name'],
+            "selected": False
+        }
+        options.append(option)
 
-	options = sorted(options, key=lambda k: k['text'])
-	if options:
-		options[0]['selected'] = True
-		dashboard['templating']['list'][0]['current'] = {
-			"tags": [],
-			"text": options[0]["text"],
-			"value": options[0]["value"]
-		}
+    options = sorted(options, key=lambda k: k['text'])
+    if options:
+        options[0]['selected'] = True
+        dashboard['templating']['list'][0]['current'] = {
+            "tags": [],
+            "text": options[0]["text"],
+            "value": options[0]["value"]
+        }
 
-	dashboard['templating']['list'][0]['options'] = options
-	outfile = os.path.join(outpath, filename)
-	outfiletmp = os.path.join(outpath, '%s.tmp' % (filename))
+    dashboard['templating']['list'][0]['options'] = options
+    outfile = os.path.join(outpath, filename)
+    outfiletmp = os.path.join(outpath, '%s.tmp' % (filename))
 
-	dump_json(dashboard, outfiletmp)
-	os.rename(outfiletmp, outfile)
+    dump_json(dashboard, outfiletmp)
+    os.rename(outfiletmp, outfile)
+
 
 def main():
-	if len(sys.argv) != 4:
-		print("./generate-dashboards.py NODELIST TEMPLATEPATH OUTPATH")
-		sys.exit(1)
+    if len(sys.argv) != 4:
+        print("./generate-dashboards.py NODELIST TEMPLATEPATH OUTPATH")
+        sys.exit(1)
 
-	nodelistjson = sys.argv[1]
-	templatepath = sys.argv[2]
-	outpath = sys.argv[3]
+    nodelistjson = sys.argv[1]
+    templatepath = sys.argv[2]
+    outpath = sys.argv[3]
 
-	# load
-	place_filter = json.load(open(os.path.join(templatepath, 'place_filter.json')))
-	nodelist = json.load(open(nodelistjson))
-	id_name_pairs = get_id_name_pairs(nodelist)
+    # load
+    place_filter = json.load(open(os.path.join(templatepath,
+                                               'place_filter.json')))
+    nodelist = json.load(open(nodelistjson))
+    id_name_pairs = get_id_name_pairs(nodelist)
 
-	# store
-	generate_places(place_filter, id_name_pairs, templatepath, outpath)
-	generate_nodegroup(id_name_pairs, templatepath, outpath)
-	generate_node(id_name_pairs, templatepath, outpath)
+    # store
+    generate_places(place_filter, id_name_pairs, templatepath, outpath)
+    generate_nodegroup(id_name_pairs, templatepath, outpath)
+    generate_node(id_name_pairs, templatepath, outpath)
+
 
 if __name__ == "__main__":
-	main()
+    main()
--- graphite-nodes.py
+++ graphite-nodes.py
@@ -1,44 +1,48 @@
 #!/usr/bin/python
 # -*- coding: utf-8; -*-
 
-import sys
 import json
+import sys
 import time
 
+
 def get_nodes_count(nodes):
-	onlinenodes_count = 0
-	offlinenodes_count = 0
+    onlinenodes_count = 0
+    offlinenodes_count = 0
 
-	for n in nodes['nodes']:
-		if not 'statistics' in n:
-			continue
+    for n in nodes['nodes']:
+        if 'statistics' not in n:
+            continue
 
-		if not 'flags' in n:
-			continue
+        if 'flags' not in n:
+            continue
 
-		if not 'online' in n['flags']:
-			continue
+        if 'online' not in n['flags']:
+            continue
 
-		if not n['flags']['online']:
-			offlinenodes_count += 1
-		else:
-			onlinenodes_count += 1
+        if not n['flags']['online']:
+            offlinenodes_count += 1
+        else:
+            onlinenodes_count += 1
 
-	return (onlinenodes_count, offlinenodes_count)
+    return (onlinenodes_count, offlinenodes_count)
+
 
 def main():
-	if len(sys.argv) != 2:
-		print("./graphite-nodes.py NODESJSON")
-		sys.exit(1)
+    if len(sys.argv) != 2:
+        print("./graphite-nodes.py NODESJSON")
+        sys.exit(1)
 
-	nodesjson = sys.argv[1]
+    nodesjson = sys.argv[1]
 
-	# load
-	nodes = json.load(open(nodesjson))
-	(onlinenodes_count, offlinenodes_count) = get_nodes_count(nodes)
+    # load
+    nodes = json.load(open(nodesjson))
+    (onlinenodes_count, offlinenodes_count) = get_nodes_count(nodes)
 
-	timestamp = int(time.time())
-	print("freifunk.global.offlinenodes %u %u" % (offlinenodes_count, timestamp))
+    timestamp = int(time.time())
+    print("freifunk.global.offlinenodes %u %u" % (offlinenodes_count,
+                                                  timestamp))
+
 
 if __name__ == "__main__":
-	main()
+    main()
